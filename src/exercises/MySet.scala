5aedea52e4a9d85b1865ad67adb23120450ba8f4
Demigod21 <davide.falcone276@gmail.com>
2023-04-23 19:24:13 +0200

    The functional Set, I have no idea what I'm doing

--- src/exercises/MySet.scala
+++ src/exercises/MySet.scala
@@ -28,6 +28,20 @@ trait MySet[A] extends (A => Boolean) {
   def filter(predicate: A => Boolean): MySet[A]
   def foreach(f: A => Unit): Unit
 
+  /**
+   * EXERCISE
+   * - removing element
+   * - intersection
+   * - difference
+   */
+
+  def -(elem: A) : MySet[A]
+  def &(anotherSet: MySet[A]) : MySet[A]
+  def --(anotherSet: MySet[A]) : MySet[A]
+
+  //exercise #3
+  def unary_! : MySet[A]
+
 }
 
 class EmptySet[A] extends MySet[A]{
@@ -45,6 +59,39 @@ class EmptySet[A] extends MySet[A]{
 
   override def foreach(f: A => Unit): Unit = ()
 
+  override def -(elem: A) : MySet[A] = this
+
+  override def &(anotherSet: MySet[A]) : MySet[A] = this
+
+  override def --(anotherSet: MySet[A]) : MySet[A] = this
+
+  override def unary_! : MySet[A] = new PropertyBasedSet[A](_ => true)
+
+}
+
+// all elements of type A that satisfy a property
+// { x in A | property(x) }
+class PropertyBasedSet[A](property: A => Boolean) extends MySet[A]{
+
+  override def contains(elem: A): Boolean = property(elem)
+
+  override def +(elem: A): MySet[A] = new PropertyBasedSet[A]( x => property(x) || x == elem)
+
+  override def ++(anotherSet: MySet[A]): MySet[A] = new PropertyBasedSet[A]( x => property(x) || anotherSet(x))
+
+  override def map[B](f: A => B): MySet[B] = politelyFail
+  override def flatMap[B](f: A => MySet[B]): MySet[B] = politelyFail
+  override def foreach(f: A => Unit): Unit = politelyFail
+
+  override def filter(predicate: A => Boolean): MySet[A] = new PropertyBasedSet[A](x => property(x) && predicate(x))
+
+  override def -(elem: A): MySet[A] = filter(x => x != elem)
+  override def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
+  override def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
+
+  override def unary_! : MySet[A] = new PropertyBasedSet[A](x => !property(x))
+
+  def politelyFail = throw new IllegalArgumentException("Really deep rabbit hole!")
 }
 
 class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A]{
@@ -81,6 +128,14 @@ class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A]{
     f(head)
     tail.foreach(f)
   }
+
+  override def -(elem: A) : MySet[A] = if(head == elem) tail else tail - elem  + head
+
+  override def &(anotherSet: MySet[A]) : MySet[A] = this.filter(x => anotherSet.contains(x))
+
+  override def --(anotherSet: MySet[A]) : MySet[A] = this.filter(x => !anotherSet.contains(x))
+
+  override def unary_! : MySet[A] = new PropertyBasedSet[A](x => !this.contains(x))
 }
 
 object MySet {
@@ -93,4 +148,20 @@ object MySet {
     }
     buildSet(values.toSeq, new EmptySet[A])
   }
+}
+
+object MySetPlayground extends App{
+  val s = MySet(1,2,3,4)
+
+  val negative = !s
+  println(negative(2)) // false
+  println(negative(5)) // true
+
+  val negativeEven = negative.filter(_ % 2 == 0)
+  println(negativeEven(5)) //false
+
+  val negativeEven5 = negativeEven + 5
+
+  println(negativeEven5(5)) //true
+
 }
